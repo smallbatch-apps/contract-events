a6acca4afb363d53484b997d7554efa4c58b4e10
Matt Burgess <matt@smallbatch.io>
2018-09-14 00:25:26 +1000

    Use require rather than es6 imports to facilitate ease of use with truffle

--- index.js
+++ index.js
@@ -1,75 +1,74 @@
-export default class DebugEvents {
+class DebugEvents {
 
-  constructor(contract, tx = null){
+    constructor(contract, tx = null) {
       this.contract = contract;
       this.events = this.contractEvents();
       this.tx = tx;
-  }
+    }
 
-  setTx(tx){
+    setTx(tx) {
       this.tx = tx;
       return this;
-  }
+    }
 
-  zip(keys, values) {
-      return keys.reduce((obj, k, i) => ({...obj, [k]: values[i] }), {});
-  }
-
-  getContractEvents() {
+    getContractEvents() {
       return this.events;
-  }
+    }
 
-  contractEvents(){
+    contractEvents() {
       let eventDefinitions = {};
       let events = Object.entries(this.contract.events).map(([hash, event]) => {
-          event.inputs.forEach(({indexed, name, type}) => {
-              if (!eventDefinitions.hasOwnProperty(event.name)) {
-                  eventDefinitions[event.name] = {};
-              }
-              eventDefinitions[event.name][name] = {type, indexed};
-          });
+        event.inputs.forEach(({ indexed, name, type }) => {
+          if (!eventDefinitions.hasOwnProperty(event.name)) {
+            eventDefinitions[event.name] = {};
+          }
+          eventDefinitions[event.name][name] = { type, indexed };
+        });
       });
       return eventDefinitions;
-  }
+    }
 
-  getEvent(eventName = false) {
+    getEvent(eventName = false) {
       return this.getEvents(eventName)[0];
-  }
+    }
 
-  getEvents(eventName = false) {
-      if(!this.tx){
-          throw 'There is no transaction to get events from';
+    getEvents(eventName = false) {
+      if (!this.tx) {
+        throw 'There is no transaction to get events from';
       }
-      
+
       let logs = this.tx.logs;
-      
-      if (eventName) {
-          logs = logs.filter(({event}) => event === eventName);
-      }
-      
-      return logs.map(({event, args}) => {
-              let keys = Object.keys(args);
-              let values = Object.values(args)
-                  .map((value, index) => this.cleanValue(value, this.events[event][keys[index]].type));
-              
-              return Object.assign({event}, this.zip(keys, values));
-          });
-  }   
 
-  cleanValue(value, type='bytes32'){
+      if (eventName) {
+        logs = logs.filter(({ event }) => event === eventName);
+      }
+
+      return logs.map(({ event, args }) => {
+        return Object.entries(args)
+          .filter(([key]) => isNaN(key) && key !== '__length__')
+          .reduce((obj, [key, value]) => {
+            obj[key] = this.cleanValue(value, this.events[event][key].type)
+            return obj;
+          }, {});
+      });
+    }
+
+    cleanValue(value, type = 'bytes32') {
       if (type === 'bytes32') {
-          return this.cleanString(value);
+        return this.cleanString(value);
       } else if (type.includes('uint')) {
-          return this.cleanUint(value);
+        return this.cleanUint(value);
       }
       return value;
-  }
+    }
 
-  cleanString(value){
-      return web3.toAscii(value).replace(/\u0000/g, '');
-  }
+    cleanString(value) {
+      return web3.utils.hexToUtf8(value).replace(/\u0000/g, '');
+    }
 
-  cleanUint(value){
+    cleanUint(value) {
       return +value;
+    }
   }
-}
+
+  module.exports = DebugEvents;
